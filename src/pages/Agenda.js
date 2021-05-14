c4965aa13f54b079014b62a58ee73d750d512cda
MoonTahoe <alex@moonhighway.com>
2021-05-13 18:01:11 -0700

    Grid Divs, initial layout

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -2,17 +2,7 @@ import { useLocation } from "react-router-dom";
 import { NavigationBar, MDX } from "../ui";
 import { BookStyles } from "../book-ui";
 import { useContentLinks, useContentFile } from "../hooks";
-
-//
-// Next..
-//
-//  [x] Handle End Screen
-//  [ ] Missing Code Formatting
-//  [ ] Specify PORT
-//  [ ] If PORT is chosen, randomize port
-//  [ ] Quickly Review Issues
-//  [ ] Send PR
-//
+import styled from "styled-components";
 
 export default function Agenda() {
   let { pathname } = useLocation();
@@ -20,12 +10,46 @@ export default function Agenda() {
   const [, , ...pathArray] = pathname.split("/");
   const [[pTopic, pRoute], [nTopic, nRoute]] = useContentLinks(pathArray);
   return (
-    <BookStyles>
-      <MDX>{md}</MDX>
-      <NavigationBar
-        prev={{ to: pRoute, text: pTopic.title }}
-        next={{ to: nRoute, text: nTopic.title }}
-      />
-    </BookStyles>
+    <Container>
+      <TopicTime />
+      <TopicIconInfo />
+      <TopicTitle />
+      <Contents>
+        <BookStyles>
+          <MDX>{md}</MDX>
+          <NavigationBar
+            prev={{ to: pRoute, text: pTopic.title }}
+            next={{ to: nRoute, text: nTopic.title }}
+          />
+        </BookStyles>
+      </Contents>
+    </Container>
   );
 }
+
+const Container = styled.article`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(9, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+`;
+
+const Contents = styled.div`
+  grid-area: 2 / 1 / 10 / 8;
+`;
+
+const TopicTime = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  background-color: orange;
+`;
+
+const TopicIconInfo = styled.div`
+  grid-area: 1 / 2 / 2 / 3;
+  background-color: lightblue;
+`;
+
+const TopicTitle = styled.div`
+  grid-area: 1 / 3 / 2 / 9;
+  background-color: lightgreen;
+`;
