ef13a9769c768f850cece7455d17791d5ab5f63b
MoonTahoe <alex@moonhighway.com>
2021-05-19 18:28:49 -0700

    NEW PICKS - Picks the first topic in a deep course

--- src/lib/pick.js
+++ src/lib/pick.js
@@ -8,16 +8,39 @@ export const searchByTitle =
 
 export function pickNextRoute(content, route, parent) {
   const [current] = route.split("/").reverse();
+
+  if (content.agenda) {
+    const [topic] = content.agenda;
+    return toRoute(route, topic.title);
+  }
+
   const index = parent.agenda.map(toUrlFriendly).indexOf(current) + 1;
+  if (index >= parent.agenda.length) {
+    return;
+  }
   const topic = parent.agenda[index];
   return toRoute(parent.title, topic.title);
 }
 
-export function pick(content) {
-  if (content.agenda) {
-    const [topic] = content.agenda;
+export function pickPreviousRoute(content, route, parent) {
+  const [current] = route.split("/").reverse();
+  const index = parent.agenda.map(toUrlFriendly).indexOf(current) - 1;
+  const topic = parent.agenda[index];
+  return toRoute(parent.title, topic.title);
+}
+
+export function pick(content, route) {
+  if (route) {
+    const [, nextRoute] = route.split("/");
+    const topic = content.agenda.find(searchByTitle(nextRoute));
     const current = toRoute(content.title, topic.title);
+    const prev = pickPreviousRoute(topic, current, content);
     const next = pickNextRoute(topic, current, content);
-    return [topic, [, current, next]];
+    return [topic, [prev, current, next]];
   }
+
+  const [topic] = content.agenda;
+  const current = toRoute(content.title, topic.title);
+  const next = pickNextRoute(topic, current, content);
+  return [topic, [, current, next]];
 }
--- src/lib/pick.test.js
+++ src/lib/pick.test.js
@@ -1,23 +1,49 @@
 import { pick } from "./pick";
 import { deepData, shallowData } from "./pick.test.data";
 
-describe("picking the first item in the course", () => {
+describe("picking the first topic in the course", () => {
   let topic, current, prev, next;
-
   beforeAll(() => {
     [topic, [prev, current, next]] = pick(shallowData);
   });
-
   it("picks correct topic", () =>
     expect(topic).toEqual({ title: "First Section" }));
   it("picks correct previous route", () => expect(prev).not.toBeDefined());
   it("picks the correct current route", () =>
     expect(current).toEqual("course-level/first-section"));
-  it("picks Second Section for next", () =>
+  it("picks the correct next route", () =>
     expect(next).toEqual("course-level/second-section"));
 });
 
-// describe("picking the first last in the course");
+describe("picking the first topic in a deep course", () => {
+  let topic, current, prev, next;
+  beforeAll(() => {
+    [topic, [prev, current, next]] = pick(deepData);
+  });
+  it("picks correct topic", () => expect(topic.title).toEqual("First Section"));
+  it("picks correct previous route", () => expect(prev).not.toBeDefined());
+  it("picks the correct current route", () =>
+    expect(current).toEqual("course-level/first-section"));
+  it("picks the correct next route", () =>
+    expect(next).toEqual("course-level/first-section/first-topic"));
+});
+
+describe("picking the last topic in the course", () => {
+  let topic, current, prev, next;
+  beforeAll(() => {
+    [topic, [prev, current, next]] = pick(
+      shallowData,
+      "course-level/third-section"
+    );
+  });
+  it("picks correct topic", () =>
+    expect(topic).toEqual({ title: "Third Section" }));
+  it("picks correct previous route", () =>
+    expect(prev).toEqual("course-level/second-section"));
+  it("picks the correct current route", () =>
+    expect(current).toEqual("course-level/third-section"));
+  it("picks the next correct route", () => expect(next).not.toBeDefined());
+});
 
 // describe("picking the next sibling item");
 
