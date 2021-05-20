c0b7eff5d62355bb9957737a69b6805a043e4535
MoonTahoe <alex@moonhighway.com>
2021-05-20 12:34:07 -0700

    flattenCourse test|code

--- src/lib/pick.js
+++ src/lib/pick.js
@@ -1,46 +1,16 @@
-export const urlFriendly = (str) => str.trim().toLowerCase().replace(/ /g, "-");
-export const toUrlFriendly = ({ title }) => urlFriendly(title);
-export const toRoute = (...titles) => titles.map(urlFriendly).join("/");
-export const searchByTitle =
-  (searchTitle) =>
-  ({ title }) =>
-    urlFriendly(title) === urlFriendly(searchTitle);
+export const titlesOnly = ({ title }) => title;
 
-export function pickNextRoute(content, route, parent) {
-  const [current] = route.split("/").reverse();
-
-  if (content.agenda) {
-    const [topic] = content.agenda;
-    return toRoute(route, topic.title);
+export function flattenCourse(course) {
+  const { agenda, ...rest } = course;
+  if (agenda) {
+    return [{ ...rest }, ...course.agenda.flatMap(flattenCourse)];
   }
-
-  const index = parent.agenda.map(toUrlFriendly).indexOf(current) + 1;
-  if (index >= parent.agenda.length) {
-    return;
-  }
-  const topic = parent.agenda[index];
-  return toRoute(parent.title, topic.title);
+  return { ...rest };
 }
 
-export function pickPreviousRoute(content, route, parent) {
-  const [current] = route.split("/").reverse();
-  const index = parent.agenda.map(toUrlFriendly).indexOf(current) - 1;
-  const topic = parent.agenda[index];
-  return toRoute(parent.title, topic.title);
-}
+export function categorizeCourse(flattenedCourse) {}
 
-export function pick(content, route) {
-  if (route) {
-    const [, nextRoute] = route.split("/");
-    const topic = content.agenda.find(searchByTitle(nextRoute));
-    const current = toRoute(content.title, topic.title);
-    const prev = pickPreviousRoute(topic, current, content);
-    const next = pickNextRoute(topic, current, content);
-    return [topic, [prev, current, next]];
-  }
-
-  const [topic] = content.agenda;
-  const current = toRoute(content.title, topic.title);
-  const next = pickNextRoute(topic, current, content);
-  return [topic, [, current, next]];
+export function pick(course) {
+  const [firstTopic] = flattenCourse(course);
+  return firstTopic;
 }
--- src/lib/pick.test.js
+++ src/lib/pick.test.js
@@ -1,58 +1,15 @@
-import { pick } from "./pick";
-import { deepData, shallowData } from "./pick.test.data";
+import { flattenCourse, titlesOnly, categorizeCourse, pick } from "./pick";
+import { sampleCourse } from "./pick.test.data";
 
-describe("picking the first topic in the course", () => {
-  let topic, current, prev, next;
-  beforeAll(() => {
-    [topic, [prev, current, next]] = pick(shallowData);
-  });
-  it("picks correct topic", () =>
-    expect(topic).toEqual({ title: "First Section" }));
-  it("picks correct previous route", () => expect(prev).not.toBeDefined());
-  it("picks the correct current route", () =>
-    expect(current).toEqual("course-level/first-section"));
-  it("picks the correct next route", () =>
-    expect(next).toEqual("course-level/second-section"));
+it("flattenCourse(course)", () => {
+  const result = flattenCourse(sampleCourse);
+  expect(result).toEqual([
+    "sample course",
+    "Chapter One",
+    "step one",
+    "task A",
+    "task B",
+    "Challenge 1",
+    "Challenge 2",
+  ]);
 });
-
-describe("picking the first topic in a deep course", () => {
-  let topic, current, prev, next;
-  beforeAll(() => {
-    [topic, [prev, current, next]] = pick(deepData);
-  });
-  it("picks correct topic", () => expect(topic.title).toEqual("First Section"));
-  it("picks correct previous route", () => expect(prev).not.toBeDefined());
-  it("picks the correct current route", () =>
-    expect(current).toEqual("course-level/first-section"));
-  it("picks the correct next route", () =>
-    expect(next).toEqual("course-level/first-section/first-topic"));
-});
-
-describe("picking the last topic in the course", () => {
-  let topic, current, prev, next;
-  beforeAll(() => {
-    [topic, [prev, current, next]] = pick(
-      shallowData,
-      "course-level/third-section"
-    );
-  });
-  it("picks correct topic", () =>
-    expect(topic).toEqual({ title: "Third Section" }));
-  it("picks correct previous route", () =>
-    expect(prev).toEqual("course-level/second-section"));
-  it("picks the correct current route", () =>
-    expect(current).toEqual("course-level/third-section"));
-  it("picks the next correct route", () => expect(next).not.toBeDefined());
-});
-
-// describe("picking the next sibling item");
-
-// describe("picking the previous sibling item");
-
-// describe("picking the next nested item");
-
-// describe("picking the previous nested item");
-
-// describe("picking the next nested sibling");
-
-// describe("picking the previous nested sibling");
