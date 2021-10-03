b669d1386640a23df3b20e6915558a192a20050c
MoonTahoe <alex@moonhighway.com>
2021-10-03 12:36:38 -0700

    BROKEN COMMIT

---
+++ src/useTimesplitter/__tests__/course-title.js
@@ -0,0 +1,16 @@
+import { renderHook } from "@testing-library/react-hooks";
+import "@testing-library/jest-dom";
+import { useTimesplitter, TimesplitterProvider } from "..";
+
+describe("useTimesplitter - title state", () => {
+  it("Injected Title Correct", () => {
+    const testContent = { title: "Test Course", agenda: [] };
+    const wrapper = ({ children }) => (
+      <TimesplitterProvider defaultContent={testContent}>
+        {children}
+      </TimesplitterProvider>
+    );
+    const { result } = renderHook(() => useTimesplitter(), { wrapper });
+    expect(result.title).toEqual("Small Sample Course");
+  });
+});
