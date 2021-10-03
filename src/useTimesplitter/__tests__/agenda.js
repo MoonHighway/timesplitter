406a391151dd90893e131067cf636d2b2b017095
MoonTahoe <alex@moonhighway.com>
2021-10-03 12:52:20 -0700

    Agenda Hook Test

--- src/useTimesplitter/__tests__/agenda.js
+++ src/useTimesplitter/__tests__/agenda.js
@@ -1,28 +1,24 @@
-describe("Fuck", () => it("YOU", () => {}));
-// import { screen } from "@testing-library/react";
-// import { useTimesplitter } from "..";
-// import { renderTimesplitter } from "../test-helpers";
+import { renderHook } from "@testing-library/react-hooks";
+import "@testing-library/jest-dom";
+import { useTimesplitter, TimesplitterProvider } from "..";
 
-// const agendaSample = {
-//   title: "Sample Course",
-//   agenda: [
-//     { title: "agenda one" },
-//     { title: "agenda two" },
-//     { title: "agenda three" },
-//   ],
-// };
-
-// function AgendaConsumer() {
-//   const { agenda } = useTimesplitter();
-//   return <pre data-testid="raw-data">{JSON.stringify(agenda, null, 2)}</pre>;
-// }
-
-// describe("useTimesplitter", () => {
-//   describe("agenda", () => {
-//     it("INITIAL", () => {
-//       renderTimesplitter(<AgendaConsumer />, agendaSample);
-//       const result = JSON.parse(screen.getByTestId("raw-data").innerHTML);
-//       expect(result).toEqual(agendaSample.agenda);
-//     });
-//   });
-// });
+describe("useTimesplitter - agenda state", () => {
+  it("Injected Agenda Correct", () => {
+    const testContent = {
+      title: "Sample Course",
+      agenda: [
+        { title: "agenda one" },
+        { title: "agenda two" },
+        { title: "agenda three" },
+      ],
+    };
+    const wrapper = ({ children }) => (
+      <TimesplitterProvider defaultContent={testContent}>
+        {children}
+      </TimesplitterProvider>
+    );
+    const { result } = renderHook(() => useTimesplitter(), { wrapper });
+    const { agenda } = result.current;
+    expect(agenda).toEqual(testContent.agenda);
+  });
+});
